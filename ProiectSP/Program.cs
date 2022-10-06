c12b2203121fd51757775e3f1c9cf51f873a669a
zaticpetru <zatic.petru@outlook.com>
2022-10-06 13:42:52 +0300

    Implement Book and BookItem class

--- ProiectSP/Program.cs
+++ ProiectSP/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var book = new Book("My book title");
+
+book.createNewParagraph("Parag");
+book.createNewTable("Table");
+book.createNewImage("Image");
+
+Console.WriteLine(book);
+
+public class Book
+{
+    public string Title { get; set; } 
+    public List<BookItem> BookItems { get; set; }
+
+    public Book(string title)
+    {
+        Title = title;
+        BookItems = new List<BookItem>();
+    }
+
+    public void createNewParagraph(string content)
+    {
+        BookItems.Add(new Paragraph(content));
+    }
+    public void createNewImage(string content)
+    {
+        BookItems.Add(new Image(content));
+    }
+    public void createNewTable(string content)
+    {
+        BookItems.Add(new Table(content));
+    }
+    public override string ToString()
+    {
+        return $"Titlu: {Title} \n{string.Join("\n", BookItems)}";
+    }
+}
+
+public class BookItem
+{
+    public string Content { get; set; }
+
+    public BookItem(string content)
+    {
+        Content = content;
+    }
+
+    public override string ToString()
+    {
+        return Content;
+    }
+}
+
+public class Paragraph : BookItem
+{
+    public Paragraph(string content) : base(content)
+    {
+    }
+}
+
+public class Image : BookItem
+{
+    public Image(string content) : base(content)
+    {
+    }
+
+    public override string ToString()
+    {
+        return Content + "* rendering *";
+    }
+}
+
+public class Table : BookItem
+{
+    public Table(string content) : base(content)
+    {
+    }
+}
+
